07f83797a97673da46f478e6e83825660ef6a240
Hidenori Miyoshi <miyohide@gmail.com>
2020-03-24 21:36:04 +0900

    move isGameEnd function to game.js

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,5 +1,6 @@
-import { takeTurn } from '../lib/game'
+import { isGameEnd, takeTurn } from '../lib/game'
 import { diskColor } from '../lib/const'
+import { initBoard } from '../lib/board'
 
 test('when turn is dark, taketurn returns light', () => {
   expect(takeTurn(diskColor.dark)).toBe(diskColor.light)
@@ -7,4 +8,22 @@ test('when turn is dark, taketurn returns light', () => {
 
 test('when turn is light, taketurn returns dark', () => {
   expect(takeTurn(diskColor.light)).toBe(diskColor.dark)
+})
+
+test('when initial board, isGameEnd returns false', () => {
+  expect(isGameEnd(initBoard())).toBeFalsy()
+})
+
+test('when all dark, isGameEnd returns true', () => {
+  const b = initBoard()
+  b[3][3] = diskColor.dark
+  b[4][4] = diskColor.dark
+  expect(isGameEnd(b)).toBeTruthy()
+})
+
+test('when all light, isGameEnd returns true', () => {
+  const b = initBoard()
+  b[4][3] = diskColor.light
+  b[3][4] = diskColor.light
+  expect(isGameEnd(b)).toBeTruthy()
 })
