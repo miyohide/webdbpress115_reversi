70e8a8cc0a61fa15968884aed608b0b845cdd11e
Hidenori Miyoshi <miyohide@gmail.com>
2020-03-26 21:38:31 +0900

    extract countDisk function.

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,4 +1,4 @@
-import { isGameEnd, takeTurn } from '../lib/game'
+import { countDisk, isGameEnd, takeTurn } from '../lib/game'
 import { diskColor } from '../lib/const'
 import { initBoard } from '../lib/board'
 
@@ -26,4 +26,22 @@ test('when all light, isGameEnd returns true', () => {
   b[4][3] = diskColor.light
   b[3][4] = diskColor.light
   expect(isGameEnd(b)).toBeTruthy()
+})
+
+test('when initial board, countDisk returns [2, 2]', () => {
+  expect(countDisk(initBoard())).toEqual([2, 2])
+})
+
+test('when all dark, countDisk returns [4, 0]', () => {
+  const b = initBoard()
+  b[3][3] = diskColor.dark
+  b[4][4] = diskColor.dark
+  expect(countDisk(b)).toEqual([4, 0])
+})
+
+test('when all light, countDisk returns [0, 4]', () => {
+  const b = initBoard()
+  b[4][3] = diskColor.light
+  b[3][4] = diskColor.light
+  expect(countDisk(b)).toEqual([0, 4])
 })
