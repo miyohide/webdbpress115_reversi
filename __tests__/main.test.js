54a5f9bc56c1b303e49c5b6ff156becfcc2b74c5
Hidenori Miyoshi <miyohide@gmail.com>
2020-04-11 14:46:10 +0900

    wキーを押したときのテストを追加

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -17,9 +17,37 @@ function initialPage () {
   return page
 }
 
+function moveUpFromInitialPage () {
+  let page = '・・・・・・・・<br>'
+  page += '・・・・・・・・<br>'
+  page += '・・・・・・・・<br>'
+  page += '・・・○●・・・<br>'
+  page += '・・・●○・・・<br>'
+  page += '・・・・・・・・<br>'
+  page += '・・・・・・・・<br>'
+  page += '・・・・・・・・←<br>'
+  page += '↑　　　　　　　<br>'
+  page += '<br>黒のターンです。<br><br>\n'
+  page += '      [w]:カーソルを上に移動<br>\n'
+  page += '      [s]:カーソルを下に移動<br>\n'
+  page += '      [a]:カーソルを左に移動<br>\n'
+  page += '      [d]:カーソルを右に移動<br>\n'
+  page += '      [その他のキー]:石を置く'
+  return page
+}
+
 test('initial load', () => {
   document.body.innerHTML = '<div></div>'
   require('../lib/main')
   const div = document.querySelector('div')
   expect(div.innerHTML).toEqual(initialPage())
+})
+
+test('when w key down, move up', () => {
+  document.body.innerHTML = '<div></div>'
+  const main = require('../lib/main')
+  const wDown = new window.KeyboardEvent('keydown', { key: 'w' })
+  main.onKeyDown(wDown)
+  const div = document.querySelector('div')
+  expect(div.innerHTML).toEqual(moveUpFromInitialPage())
 })
